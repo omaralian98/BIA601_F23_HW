ad756945c91ab7983889503740a40a5ef9b00e91
omaralian98 <omaralian9870@gmail.com>
2024-06-20 13:59:24 +0300

    Fixed Port is already in use

--- Start_Server.bat
+++ Start_Server.bat
@@ -1,20 +1,22 @@
 @echo off
+set BACKEND_PORT=5000
+
 cd Backend\CSharp\publish
-start API.exe
+start API.exe --port=%BACKEND_PORT%
 cd ../../../frontend
 setlocal
 
 :: Check for Windows
 if not "%OS%"=="" (
     echo Running on Windows
-    set REACT_APP_API_URL=http://localhost:5000
+    set REACT_APP_API_URL=http://localhost:%BACKEND_PORT%
     call npm start
     goto end
 )
 
-:: If not Windows, assume Unix-like (Linux/macOS)
+:: If not Windows
 echo Running on Unix-like OS (Linux or macOS)
-set REACT_APP_API_URL=http://localhost:5000
+set REACT_APP_API_URL=http://localhost:%BACKEND_PORT%
 npm start
 
 :end
