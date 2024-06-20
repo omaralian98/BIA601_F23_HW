214638b51473a0d935bb42a4b18d2b753a71c2dc
omaralian98 <omaralian9870@gmail.com>
2024-06-20 12:51:14 +0300

    Update Start_Server.bat

--- Start_Server.bat
+++ Start_Server.bat
@@ -1,5 +1,21 @@
 @echo off
-cd Backend\CSharp\API\bin\Release\net8.0\publish
+cd Backend\CSharp\publish
 start API.exe
-cd ../../../../../../../frontend/build
+cd ../../../frontend
+setlocal
+
+:: Check for Windows
+if not "%OS%"=="" (
+    echo Running on Windows
+    set REACT_APP_API_URL=http://localhost:5000
+    call npm start
+    goto end
+)
+
+:: If not Windows, assume Unix-like (Linux/macOS)
+echo Running on Unix-like OS (Linux or macOS)
+set REACT_APP_API_URL=http://localhost:5000
 npm start
+
+:end
+endlocal
